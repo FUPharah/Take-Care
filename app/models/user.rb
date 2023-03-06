b9da105e3de3f7b7c50acd57429088d7e4ac5bd6
canozben <canozben@windowslive.com>
2023-03-06 16:08:50 +0100

    asd

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   validates :first_name, presence: true, length: { minimum: 3 }
   validates :last_name, presence:  true, length: { minimum: 3 }
   validates :age, presence: true, numericality: { only_integer: true, greater_than: 0 }
-  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i}
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
   has_many :families
   has_one_attached :photo
 end
